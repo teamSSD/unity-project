b807c696fd5b215b2c34a4b21a74db3d36310c65
pje1122 <jeongeun1122@naver.com>
2026-01-13 05:30:00 +0900

    -

--- Assets/Scripts/Entities/Mall/delivery/DeliveryManager.cs
+++ Assets/Scripts/Entities/Mall/delivery/DeliveryManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -6,15 +7,20 @@ using Random = UnityEngine.Random;
 public class DeliveryManager : MonoBehaviour
 {
     public GameObject deliveryCustomerPrefab;
+    public GameObject receiptPrefab;
+    public GameObject waitingCustomerPrefab;
     public bool isOpen = true;
     public int deliveryOrderCount = 3;
 
+    private List<EntryDto> waitingCustomers;
     private List<MenuSchema> salesMenus;
     private int nextOrderingNumber = 1;
-
+    private GameObject orderingCustomer;
 
     void Awake()
     {
+        waitingCustomers = new List<EntryDto>();
+
         TempSearchFoodUsecase tempSearchFoodUsecase = new TempSearchFoodUsecase();
 
         salesMenus = new List<MenuSchema>
@@ -44,7 +50,7 @@ public class DeliveryManager : MonoBehaviour
         }
     }
 
-    private void GenerateCustomer()
+    private GameObject GenerateCustomer()
     {
         Vector3 spawnPos = new Vector3(
             Random.Range(0f, 20f), // x: 0~20 랜덤
@@ -61,13 +67,31 @@ public class DeliveryManager : MonoBehaviour
         DeliveryOrderingCustomer script = ordering.GetComponent<DeliveryOrderingCustomer>();
         script.menuSchema = pickRandomMenu();
 
-        // 주문 완료 → 즉시 퇴장
         script.onExit += () =>
         {
-            Destroy(ordering);
+            orderingCustomer = null;
+            registRecieptAndWaitingCustomer(script.menuSchema);
         };
+        return ordering;
     }
 
+    private void registRecieptAndWaitingCustomer(MenuSchema menuSchema)
+    {
+        GameObject receipt = Instantiate(receiptPrefab);
+        OrderTicketModel orderTicketModel = receipt.GetComponent<OrderTicketModel>();
+        Receipt recieptScript = receipt.GetComponent<Receipt>();
+
+        GameObject waitingCustomer = Instantiate(waitingCustomerPrefab);
+
+        recieptScript.Set(menuSchema);
+        orderTicketModel.waitingCustomer = waitingCustomer;
+        orderTicketModel.menuSchema = menuSchema;
+        orderTicketModel.onTake += () => StartCoroutine(RunNextFrame(() =>
+        {
+            //clearWaitingCustomer();
+            //checkEnd();
+        }));
+    }
 
     private MenuSchema pickRandomMenu()
     {
@@ -75,5 +99,20 @@ public class DeliveryManager : MonoBehaviour
         menuSchema.orderNumber = nextOrderingNumber;
         nextOrderingNumber++;
         return menuSchema;
+    }
+
+    IEnumerator RunNextFrame(Action action)
+    {
+        yield return null;
+        action?.Invoke();  
+    }
+    private class EntryDto
+    {
+        public GameObject reciept;
+        public EntryDto() { }
+        public EntryDto(GameObject reciept)
+        {
+            this.reciept = reciept;
+        }
     }
 }
