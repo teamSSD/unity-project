2eecc54f2251b4cc7cdaa31f7c6a72a0ef51e9d0
pje1122 <jeongeun1122@naver.com>
2026-01-12 19:18:41 +0900

    -

---
+++ Assets/Scripts/Entities/Mall/delivery/DeliveryManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class DeliveryManager : MonoBehaviour
+{
+    public GameObject receiptPrefab;
+
+    /// <summary>
+    /// 주문 완료 시 호출
+    /// </summary>
+    public GameObject CreateReceipt(MenuSchema menuSchema)
+    {
+        GameObject receipt = Instantiate(receiptPrefab);
+
+        Receipt receiptUI = receipt.GetComponent<Receipt>();
+        OrderTicketModel ticket = receipt.GetComponent<OrderTicketModel>();
+
+        receiptUI.Set(menuSchema);
+        ticket.menuSchema = menuSchema;
+
+        return receipt;
+    }
+}
