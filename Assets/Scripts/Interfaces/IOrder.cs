650fe6e3c30d2943a0f1e246ceb3737fa733b9a0
pje1122 <jeongeun1122@naver.com>
2026-01-14 02:58:53 +0900

    -

---
+++ Assets/Scripts/Interfaces/IOrder.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+public interface IOrder
+{
+    int GenerateOrder(MenuSchema menu, string questId);
+    bool IsOrderComplete(string questId);
+    int ConsumeBento(string questId);
+}
