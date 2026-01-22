c86cc91c0e9d32ec74b8d03458ad557f1045d793
pje1122 <jeongeun1122@naver.com>
2026-01-23 05:06:01 +0900

    -

--- Assets/Scripts/Entities/Mall/delivery/DeliveryOrderingCustomer.cs
+++ Assets/Scripts/Entities/Mall/delivery/DeliveryOrderingCustomer.cs
@@ -1,58 +1,105 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(ClickStateUtil))]
 public class DeliveryOrderingCustomer : MonoBehaviour
 {
-    [Header("주입해야할 필드")]
+    [Header("프리팹")]
     public GameObject speechBubblePrefab;
-    public MenuSchema menuSchema;
-    public event Action onExit;
+    public GameObject receiptPrefab;
 
-    [Header("내부적 속성")]
-    //private ClickStateUtil clickStateUtil;
+    [Header("상태")]
     private bool isDisplaying = false;
     private GameObject speechBubble;
-    SpeechBubble speechBubbleScript;
+    private SpeechBubble speechBubbleScript;
 
-    /*
-    public void Awake()
+    private MenuSchema menuSchema;
+
+    private static int nextOrderingNumber = 1;
+
+    void Awake()
     {
-        clickStateUtil = GetComponent<ClickStateUtil>();
-        clickStateUtil.OnClicked += clickRoutine;
+        menuSchema = GenerateRandomMenu();
     }
-    */
-    public void OnDestroy()
+
+    void OnDestroy()
     {
-        //clickStateUtil.OnClicked -= clickRoutine;
-        if (speechBubble != null) Destroy(speechBubble);
+        if (speechBubble != null)
+            Destroy(speechBubble);
     }
-    void OnTriggerEnter2D(Collider2D other)//클릭 잘 안돼서 잠시 충돌로 대체 (임시)
+
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
-        clickRoutine(); 
+
+        ClickRoutine();
     }
 
-    public void clickRoutine()
+    private void ClickRoutine()
     {
         if (isDisplaying)
         {
+            CreateOrder();
             Destroy(gameObject);
-            onExit.Invoke();
             return;
         }
-        say("배달주문이요! " + menuSchema.name + " 하나 부탁드릴게요");
 
+        Say($"배달주문이요! {menuSchema.name} 하나 부탁드릴게요");
         isDisplaying = true;
     }
 
-    private void say(string message)
+    private void CreateOrder()
+    {
+        string questId = Guid.NewGuid().ToString();
+
+        OrderManager.Instance.GenerateOrder(menuSchema, questId);
+        CreateReceipt();
+    }
+
+    private void CreateReceipt()
+    {
+        GameObject receipt = Instantiate(receiptPrefab);
+
+        Receipt receiptScript = receipt.GetComponent<Receipt>();
+        OrderTicketModel ticketModel = receipt.GetComponent<OrderTicketModel>();
+
+        receiptScript.Set(menuSchema);
+        ticketModel.menuSchema = menuSchema;
+    }
+
+    private void Say(string message)
     {
         speechBubble = Instantiate(speechBubblePrefab);
-
         speechBubbleScript = speechBubble.GetComponent<SpeechBubble>();
+
         speechBubbleScript.setContents(message);
-        speechBubble.transform.position = this.transform.position + new Vector3(-2.5f, 4, 0);
+        speechBubble.transform.position =
+            transform.position + new Vector3(-2.5f, 4f, 0f);
+    }
+
+    private MenuSchema GenerateRandomMenu()
+    {
+        TempSearchFoodUsecase tempSearchFoodUsecase = new TempSearchFoodUsecase();
+
+        List<MenuSchema> menus = new List<MenuSchema>
+        {
+            new MenuSchema(
+                "임시정식 A",
+                -1,
+                tempSearchFoodUsecase.Search("I034"),
+                new List<FoodData>
+                {
+                    tempSearchFoodUsecase.Search("I046"),
+                    tempSearchFoodUsecase.Search("I058"),
+                    tempSearchFoodUsecase.Search("I062")
+                }
+            ),
+        };
+
+        MenuSchema picked = menus[Random.Range(0, menus.Count)];
+        picked.orderNumber = nextOrderingNumber++;
+        return picked;
     }
 }
