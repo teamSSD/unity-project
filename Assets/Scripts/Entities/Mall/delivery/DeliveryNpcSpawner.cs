c52d18df6091b64475d5a2ba94db3281703f769c
pje1122 <jeongeun1122@naver.com>
2026-01-24 04:52:48 +0900

    -

---
+++ Assets/Scripts/Entities/Mall/delivery/DeliveryNpcSpawner.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class DeliveryNpcSpawner : MonoBehaviour
+{
+    [SerializeField] private DeliveryNpcView npcPrefab;
+    [SerializeField]private string csvPath = "driveAssets/dataTables/deliveryNPC";
+
+
+    private readonly List<GameObject> spawnedNpcs = new();
+
+    private void Start()
+    {
+        SpawnAll();
+    }
+
+    public void SpawnAll()
+    {
+        Clear();
+
+        List<DeliveryNpcCsvData> npcDatas =
+            DeliveryNpcCsvLoader.Load(csvPath);
+
+        foreach (var data in npcDatas)
+        {
+            // 수령 완료는 안 보여줄 수도 있음
+            if (data.state == DeliveryNpcState.Completed)
+                continue;
+
+            DeliveryNpcView npc =
+                Instantiate(npcPrefab, transform);
+
+            npc.Init(data);
+            spawnedNpcs.Add(npc.gameObject);
+        }
+    }
+
+    private void Clear()
+    {
+        foreach (var npc in spawnedNpcs)
+            Destroy(npc);
+
+        spawnedNpcs.Clear();
+    }
+}
---
+++ Assets/Scripts/Entities/Mall/delivery/DeliveryNpcView.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class DeliveryNpcView : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    public void Init(DeliveryNpcCsvData data)
+    {
+        spriteRenderer.sprite =
+            Resources.Load<Sprite>(data.spritePath);
+
+        transform.position = data.position;
+
+        ApplyState(data.state);
+    }
+
+    private void ApplyState(DeliveryNpcState state)
+    {
+        switch (state)
+        {
+            case DeliveryNpcState.Orderable:
+                spriteRenderer.color = Color.white;
+                break;
+
+            case DeliveryNpcState.WaitingReceipt:
+                spriteRenderer.color = Color.yellow;
+                break;
+
+            case DeliveryNpcState.Completed:
+                spriteRenderer.color = Color.gray;
+                break;
+        }
+    }
+}
