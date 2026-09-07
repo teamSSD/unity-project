eb626ea01446e3a44572a5f91cd7a45c6b925013
moongua404 <hogun.soongit@gmail.com>
2026-09-07 17:51:41 +0900

    fix: place farm labels above crops

--- Assets/Tests/EditMode/Systems/FarmTileLayoutContractTest.cs
+++ Assets/Tests/EditMode/Systems/FarmTileLayoutContractTest.cs
@@ -16,10 +16,10 @@ public class FarmTileLayoutContractTest
         var nameCanvas = Regex.Match(prefab,
             @"m_Father: \{fileID: 2423252458153559830\}[\s\S]*?m_AnchoredPosition: \{x: 0, y: (-?\d+)\}[\s\S]*?m_SizeDelta: \{x: 400, y: (\d+)\}");
         Assert.That(nameCanvas.Success, Is.True, "FarmTile 작물명 캔버스의 레이아웃을 찾지 못했습니다.");
-        Assert.That(int.Parse(nameCanvas.Groups[1].Value), Is.LessThanOrEqualTo(-170),
-            "작물명은 작물 스프라이트 아래에 배치되어야 합니다.");
-        Assert.That(int.Parse(nameCanvas.Groups[2].Value), Is.LessThanOrEqualTo(80),
-            "작물명 영역은 작물과 겹치지 않도록 컴팩트해야 합니다.");
+        Assert.That(int.Parse(nameCanvas.Groups[1].Value), Is.GreaterThanOrEqualTo(150),
+            "작물명은 작물 스프라이트 바로 위에 배치되어야 합니다.");
+        Assert.That(int.Parse(nameCanvas.Groups[2].Value), Is.EqualTo(100),
+            "작물명 영역은 원래의 가독성 있는 높이를 유지해야 합니다.");
 
         var gaugeWidth = Regex.Match(prefab,
             @"propertyPath: m_SizeDelta.x\s+value: (\d+)[\s\S]*?propertyPath: m_SizeDelta.y\s+value: (\d+)");
